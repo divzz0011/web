0898f63242ca54dcfb008646cf3bed1131bd3509
divzz0011 <divzzxxx@gmail.com>
2025-03-12 19:43:56 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function updateProfile() {
+    document.getElementById("name").textContent = "ORDER JASA";
+    document.getElementById("position").textContent = "Order? hubungi melaui platform di bawah";
+    document.getElementById("email").textContent = "divzzxxx@gmail.com";
+    document.getElementById("phone").textContent = "+62 8181 8160 725";
+    document.getElementById("address").textContent = "Jl. ytta, Bandung";
+    document.getElementById("instagram").href = "https://instagram.com/@divzz_neverlouse";
+    document.getElementById("instagram").textContent = "@Divzz";
+}
